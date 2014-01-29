295e05b5ff240f9049c0bbab49251316201cc786
Peter Ivanov <peter@microweber.com>
2014-01-29 13:49:42 +0200

    update

--- index.php
+++ index.php
@@ -28,16 +28,8 @@ $this_file_link='https://github.com/microweber/microweber-docs/tree/master/'.url
 
 
     $(document).ready(function(){
-        
-
-		
 		prettyPrint();
-		
-		
     });
-
-
-
 
      $(window).bind("load resize", function(){
        $(document.getElementById('sidebar-wrapper-holder')).css({
@@ -46,12 +38,8 @@ $this_file_link='https://github.com/microweber/microweber-docs/tree/master/'.url
        });
      })
 
-
-
-
     </script>
     </head>
-
     <body>
     <script>
     if('ontouchstart' in document.documentElement){
@@ -59,12 +47,7 @@ $this_file_link='https://github.com/microweber/microweber-docs/tree/master/'.url
     }
     </script>
 	<div id="wrapper">
-
-		<!-- Sidebar -->
-
-
 		<div id="sidebar-wrapper">
-
         <span id="fader"></span>
 		<div id="sidebar-wrapper-holder">
 
@@ -72,44 +55,17 @@ $this_file_link='https://github.com/microweber/microweber-docs/tree/master/'.url
 
 			<ul class="sidebar-nav">
 				<li class="sidebar-brand">
-
                   <div id="logotholder"><a href="<?php print site_url(); ?>" id="logot">API Documentation</a></div>
-
-
                 </li>
-
-
-
 				<li><a class="nav-title <?php if(url_path() == 'functions/_nav/content') print "active" ; ?>" href="<?php print site_url(); ?>functions/_nav/content">Content</a> <?php print page_content('functions/_nav/content'); ?> </li>
-				
-				
 				<li><a class="nav-title <?php if(url_path() == 'functions/_nav/categories') print "active" ; ?>" href="<?php print site_url(); ?>functions/_nav/categories">Categories</a> <?php print page_content('functions/_nav/categories'); ?> </li>
-
-
 				<li><a class="nav-title <?php if(url_path() == 'functions/_nav/url') print "active" ; ?>" href="<?php print site_url(); ?>functions/_nav/url">URL</a> <?php print page_content('functions/_nav/url'); ?> </li>
-				
-				
 				<li><a class="nav-title <?php if(url_path() == 'functions/_nav/template') print "active" ; ?>" href="<?php print site_url(); ?>functions/_nav/template">Template</a> <?php print page_content('functions/_nav/template'); ?> </li>
-				
-				
-						
 				<li><a class="nav-title <?php if(url_path() == 'functions/_nav/options') print "active" ; ?>" href="<?php print site_url(); ?>functions/_nav/options">Options</a> <?php print page_content('functions/_nav/options'); ?> </li>
-			
 				<li><a class="nav-title <?php if(url_path() == 'functions/_nav/shop') print "active" ; ?>" href="<?php print site_url(); ?>functions/_nav/shop">Shop</a> <?php print page_content('functions/_nav/shop'); ?> </li>
-				
 				<li><a class="nav-title <?php if(url_path() == 'functions/_nav/users') print "active" ; ?>" href="<?php print site_url(); ?>functions/_nav/users">Users</a> <?php print page_content('functions/_nav/users'); ?> </li>
-				
-				
-		
-				
-			 
-				
-				
-				
 				<li><a class="nav-title <?php if(url_path() == 'functions/_nav/db') print "active" ; ?>" href="<?php print site_url(); ?>functions/_nav/db">Database</a> <?php print page_content('functions/_nav/db'); ?> </li>
-
-				
-					<li><a class="nav-title <?php if(url_path() == 'functions/_nav/session') print "active" ; ?>" href="<?php print site_url(); ?>functions/_nav/session">Sessions</a> <?php print page_content('functions/_nav/session'); ?> </li>
+				<li><a class="nav-title <?php if(url_path() == 'functions/_nav/session') print "active" ; ?>" href="<?php print site_url(); ?>functions/_nav/session">Sessions</a> <?php print page_content('functions/_nav/session'); ?> </li>
 			</ul>
 		</div>
 		</div>
