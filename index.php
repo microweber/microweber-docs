73b4a44b1de6be5e09c8009fcfa8f79e80103b61
Peter Ivanov <peter@microweber.com>
2014-03-12 19:52:28 +0200

    added comments module docs

--- index.php
+++ index.php
@@ -33,7 +33,6 @@ if(is_ajax()){
 <title><?php print $title; ?></title>
 <meta name="description" content="<?php print $description; ?>">
 <meta name="author" content="Microweber">
-
 <script src="<?php print site_url(); ?>assets/js/jquery.js"></script>
 <link href="<?php print site_url(); ?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <link href="<?php print site_url(); ?>assets/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
@@ -43,8 +42,7 @@ if(is_ajax()){
 <link href="<?php print site_url(); ?>assets/labels.css" rel="stylesheet">
 <script src="<?php print site_url(); ?>assets/js/highlight/highlight.pack.js"></script>
 
-
-  <!--      <link href="<?php print site_url(); ?>assets/ace-editor/api/resources/csses/ace_api.css" rel="stylesheet" type="text/css" />
+<!--      <link href="<?php print site_url(); ?>assets/ace-editor/api/resources/csses/ace_api.css" rel="stylesheet" type="text/css" />
 
   <script src="<?php print site_url(); ?>assets/ace-editor/build/src-min/ace.js"></script>
         <script src="<?php print site_url(); ?>assets/ace-editor/build/src-min/ext-static_highlight.js"></script>
@@ -54,81 +52,84 @@ if(is_ajax()){
 </script>
 <script src="<?php print site_url(); ?>assets/docs.js"></script>
 <script src="<?php print site_url(); ?>assets/runner.js"></script>
-
 </head>
 <body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=452988088116809";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <div id="wrapper">
   <div id="header">
-
- <header class="navbar navbar-static-top bs-docs-nav">
-  <div class="container">
-    <div class="navbar-header">
-      <button data-target=".bs-navbar-collapse" data-toggle="collapse" type="button" class="navbar-toggle">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php print site_url(); ?>" id="logo">Microweber Docs</a>
-    </div>
-    <nav role="navigation" class="collapse navbar-collapse bs-navbar-collapse">
-
-    <div id="search" class="pull-right">
-        <input tabindex="1" type="text" autocomplete="off" class="form-control" placeholder="Search" id="searchfield" />
-        <span class="glyphicon glyphicon-search"></span>
-        <div id="search_autocomplete"></div>
-
-
-    </div>
-
-      <ul class="nav navbar-nav">
-        <?php print page_content('header_nav'); ?>
-      </ul>
-
-    </nav>
+    <header class="navbar navbar-static-top bs-docs-nav">
+      <div class="container">
+        <div class="navbar-header">
+          <button data-target=".bs-navbar-collapse" data-toggle="collapse" type="button" class="navbar-toggle"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+          <a class="navbar-brand" href="<?php print site_url(); ?>" id="logo">Microweber Docs</a> </div>
+        <nav role="navigation" class="collapse navbar-collapse bs-navbar-collapse">
+          <div id="search" class="pull-right">
+            <input tabindex="1" type="text" autocomplete="off" class="form-control" placeholder="Search" id="searchfield" />
+            <span class="glyphicon glyphicon-search"></span>
+            <div id="search_autocomplete"></div>
+          </div>
+          <ul class="nav navbar-nav">
+            <?php print page_content('header_nav'); ?>
+          </ul>
+        </nav>
+      </div>
+    </header>
   </div>
-</header>
-  </div>
-
   <div id="content" class="well">
     <div class="container">
-       <div class="row">
-          <div class=" col-md-3 " id="sidecell">
-            <div id="sidenav">
-                <div id="sidenavcontent">
-                <?php $seg = url_segment(0); ?>
-                
-                 <div class="nav-label-color <?php print "label-".$seg; ?>"></div>
-                 
-                 <?php $seg_title =  ucwords(str_replace('-', ' ',$seg));
+      <div class="row">
+        <div class=" col-md-3 " id="sidecell">
+          <div id="sidenav">
+            <div id="sidenavcontent">
+              <?php $seg = url_segment(0); ?>
+              <div class="nav-label-color <?php print "label-".$seg; ?>"></div>
+              <?php $seg_title =  ucwords(str_replace('-', ' ',$seg));
 				 if($seg_title == ''){
 					$seg_title = "Index"; 
 				 }
 				 
 				  ?>
-                 
-                  <div id="section-title"><h3><?php print $seg_title; ?></h3></div>
-                 
-                  <div id="sidenav-menu">
-                      <?php print page_content('sidebar'); ?>
+              <div id="section-title">
+                <h3><?php print $seg_title; ?></h3>
+              </div>
+              <div id="sidenav-menu"> <?php print page_content('sidebar'); ?> </div>
+              <div id="select_holder">
+                <div class="row" >
+                  <div class="col-xs-2">
+                    <label style="padding-top: 5px;">Sections</label>
                   </div>
-                  <div id="select_holder">
-                  <div class="row" >
-                    <div class="col-xs-2">
-                        <label style="padding-top: 5px;">Sections</label>
-                    </div>
-                    <div class="col-xs-10">
-                        <div id="select_container"></div></div>
-                    </div>
-                    </div>
+                  <div class="col-xs-10">
+                    <div id="select_container"></div>
+                  </div>
                 </div>
+              </div>
             </div>
           </div>
-          <div class=" col-md-9"><div id="main-content"><?php print $content; ?></div></div>
-       </div>
+        </div>
+        <div class=" col-md-9">
+          <div id="main-content"><?php print $content; ?>
+          
+          
+          
+          <div  >
+          <hr />
+          <h4>Leave your comment</h4>
+         <div class="fb-comments" data-href="<?php print current_url(); ?>" data-numposts="66"  data-colorscheme="light" data-width="500"></div>
+          </div>
+          
+          </div>
+          
+        </div>
+      </div>
     </div>
   </div>
-
 </div>
 </body>
 </html>
--- modules/_nav/default.php
+++ modules/_nav/default.php
@@ -5,4 +5,5 @@
   <li><a class="<?php if(url_path() == 'modules/categories') print "active" ; ?>" href="<?php print site_url(); ?>modules/categories">categories</a></li>
   <li><a class="<?php if(url_path() == 'modules/menu') print "active" ; ?>" href="<?php print site_url(); ?>modules/menu">menu</a></li>
   <li><a class="<?php if(url_path() == 'modules/video') print "active" ; ?>" href="<?php print site_url(); ?>modules/video">video</a></li>
+    <li><a class="<?php if(url_path() == 'modules/comments') print "active" ; ?>" href="<?php print site_url(); ?>modules/comments">comments</a></li>
 </ul>
