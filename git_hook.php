5743a7121505b36343de4c503fcc680e417df324
Peter Ivanov <peter@microweber.com>
2018-07-10 13:37:31 +0300

    update

--- git_hook.php
+++ git_hook.php
@@ -2,5 +2,5 @@
 //exec( 'cd '.__DIR__.' && git clean -f && git reset --hard HEAD && git pull' );
 //if (isset( $_POST['payload']) ) {
   // exec( 'cd '.__DIR__.' && git clean -f && git reset --hard HEAD && git fetch && git pull' );
-   exec( 'cd '.__DIR__.'; git pull' );
+     passthru('git pull' );
 //}
